a5a5d6c61436cc3d1cd8f248e6d5112411e41456
Oleksandr Garmen <148455522+OleksandrGarmen@users.noreply.github.com>
2025-04-10 13:07:54 +0300

    Додав файл main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 puts "Введи перше число:"
-a = gets.to_f
+a = gets.to_i
 puts "Введи оператор (+, -, *, /):"
 b = gets.chomp
 puts "Введи друге число:"
-c = gets.to_f
+c = gets.to_i
 
 result = a.send(b, c) rescue "Помилка"
 
